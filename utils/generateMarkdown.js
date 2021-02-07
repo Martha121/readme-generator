dc0f571bd9cbccfd7c40cccfb04ec9768f792e72
MGcommits <marthagamez211@gmail.com>
2021-02-06 23:01:34 -0600

    Refactor code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -69,10 +69,6 @@ function renderLicenseLink(license) {
   return link;
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return ` 
@@ -94,11 +90,11 @@ function generateMarkdown(data) {
    ${data.usage}
   ## Questions
   ${data.username}
-  Link to my GitHub profile:'https://github.com/Martha121'
+  Link to my GitHub profile:https://github.com/Martha121 <br/>
   If you have questions please contactme me at:${data.email}
 
   ## License
-  This application is covered by the ${data.license} license.
+  This application is covered by the ${data.license} license.<br/>
   Link: ` + renderLicenseLink(data.license) + `. 
 `;
 }
